f83eddb42251862ae19368e46ac10ebec5514cbf
lai-atsu <atsushi.segawa729@gmail.com>
2019-01-17 21:28:43 +0900

    commit

--- trainapp/app.js
+++ trainapp/app.js
@@ -3,7 +3,13 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
-
+const session = require('express-session');
+const session_opt = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 }
+};
 const mysql = require('mysql');
 var mysql_setting = {
     host    : 'localhost',
@@ -11,16 +17,41 @@ var mysql_setting = {
     password: '',
     database: 'train-db',
 }
-
-
+app.use(session(session_opt));
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    var data ={
-        title:'hello',
-        content:'てすとやで'
-    }
-    res.render('index.ejs',data);
+  var data ={
+      title:'hello',
+      content:'てすとやで'
+  }
+  res.render('index.ejs',data);
+});
+
+app.get('/home', (req,res) => {
+  //login status
+  var status;
+  if (req.session.login == null) {
+    //login
+    status = false;
+    console.log('test');
+  }
+    res.render('home.ejs', status);
+});
+
+app.get('/chat', (req,res) => {
+    res.render('chat.ejs');
+});
+
+app.get('/bbs', (req,res) => {
+    res.render('bbs.ejs');
+});
+
+app.get('/profile', (req,res) => {
+    res.render('profile.ejs');
+});
+app.get('/login', (req,res) => {
+    res.render('login.ejs');
 });
 
 
@@ -38,7 +69,7 @@ io.on('connection',(socket) => {
         var connection = mysql.createConnection(mysql_setting);
         connection.connect();
         connection.query('insert into first_location3 set ?', data,(error, results,fields) => {
-            
+
         });
 
         connection.query('SELECT * from first_location3 where ido<=? and ido>=? and keido<=? and keido>=? and time<=? and time>=?',
@@ -57,7 +88,7 @@ io.on('connection',(socket) => {
         });
 
         connection.end();
-        
+
     })
 })
 
